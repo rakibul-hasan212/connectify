a68afa6efa78deb31606e8f6a649d81013b4605a
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-05-13 22:48:43 +0600

    create UserModel

---
+++ lib/features/auth/model/user_model.dart
@@ -0,0 +1,39 @@
+
+import 'dart:ui';
+
+class UserModel{
+  final String username;
+  final String email;
+  final String uid;
+  final String bio;
+  final Image image;
+  UserModel({
+    required this.email,
+    required this.username,
+    required this.uid,
+    required this.bio,
+    required this.image
+  });
+
+  factory UserModel.fromMap(Map<String, dynamic> map){
+    return UserModel(
+        email: map['email'] ?? ' ',
+        username: map['username'] ?? ' ',
+        uid: map['uid'] ?? ' ',
+        bio: map['bio'] ?? '',
+        image:  map['image']
+    );
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      'email': email,
+      'username': username,
+      'uid': uid,
+      'bio': bio,
+      'image': image
+    };
+  }
+
+
+}
